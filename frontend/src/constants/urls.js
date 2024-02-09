9b3ce8b666489f2b667184b9351001c78c7ca22a
zakisudev@gmail.com <zakisu250@gmail.com>
2024-02-09 12:01:23 +0300

    Add TODO API endpoints

--- frontend/src/constants/urls.js
+++ frontend/src/constants/urls.js
@@ -1,1 +1,2 @@
 export const USER_URLS = '/api/users';
+export const TODO_URLS = '/api/todos';
--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { USER_URLS } from '../constants/urls';
+import { TODO_URLS, USER_URLS } from '../constants/urls';
 
 // register api call
 export const registerUser = async (data) => {
@@ -30,11 +30,82 @@ export const loginUser = async (data) => {
   }
 };
 
+// logout api call
 export const logoutUser = async () => {
   try {
     const res = await axios.get(`${USER_URLS}/logout`);
     if (res?.data && res?.data?.success) {
       localStorage.clear();
+      return res?.data;
+    } else {
+      return res?.data;
+    }
+  } catch (error) {
+    return error?.response?.data;
+  }
+};
+
+// Create todo api call
+export const createTodo = async (data) => {
+  try {
+    const res = await axios.post(TODO_URLS, data);
+    if (res?.data && res?.data?.success) {
+      return res?.data;
+    } else {
+      return res?.data;
+    }
+  } catch (error) {
+    return error?.response?.data;
+  }
+};
+
+// Get all todos api call
+export const getTodos = async () => {
+  try {
+    const res = await axios.get(TODO_URLS);
+    if (res?.data && res?.data?.success) {
+      return res?.data;
+    } else {
+      return res?.data;
+    }
+  } catch (error) {
+    return error?.response?.data;
+  }
+};
+
+// Get single todo api call
+export const getTodo = async (id) => {
+  try {
+    const res = await axios.get(`${TODO_URLS}/${id}`);
+    if (res?.data && res?.data?.success) {
+      return res?.data;
+    } else {
+      return res?.data;
+    }
+  } catch (error) {
+    return error?.response?.data;
+  }
+};
+
+// Update todo api call
+export const updateTodo = async (todoId, status) => {
+  try {
+    const res = await axios.put(`${TODO_URLS}/${todoId}`, status);
+    if (res?.data && res?.data?.success) {
+      return res?.data;
+    } else {
+      return res?.data;
+    }
+  } catch (error) {
+    return error?.response?.data;
+  }
+};
+
+// Delete todo api call
+export const deleteTodo = async (id) => {
+  try {
+    const res = await axios.delete(`${TODO_URLS}/${id}`);
+    if (res?.data && res?.data?.success) {
       return res?.data;
     } else {
       return res?.data;
