9eaa3d36700bfef67e5c6a2139ecd5f5f7658624
zakisudev@gmail.com <zakisu250@gmail.com>
2024-02-09 06:26:41 +0300

    Dependencies and spinner component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,31 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Todos from './pages/Todos';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import ProtectedRoute from './components/ProtectedRoute';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/">
+      <Route path="register" element={<Register />} />
+      <Route path="login" element={<Login />} />
+      <Route path="" element={<ProtectedRoute />}>
+        <Route path="todos" element={<Todos />} />
+      </Route>
+      <Route path="*" element={<ToastContainer />} />
+    </Route>
+  )
+);
+
 function App() {
-  return (
-    <div className="App">
-      <h1 className="text-red-500">Hello</h1>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
