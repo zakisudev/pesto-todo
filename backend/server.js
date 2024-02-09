6dcb877da10901172e09aa812e85b0a1b158f963
zakisudev@gmail.com <zakisu250@gmail.com>
2024-02-09 06:24:47 +0300

    Setup express app with cors, other middlewares and listening to port 4000

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,7 @@ const app = express();
 
 // middleware
 app.use(express.json());
+// app.use(express.bodyParser());
 app.use(cors());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
